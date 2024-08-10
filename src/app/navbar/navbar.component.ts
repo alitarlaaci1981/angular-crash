9c2c3bb34a6945f6a2c3b56692c0df1f74a9d491
ali <kemal_tar@hotmail.com>
2024-08-10 13:20:03 -0500

    new component created

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import{Component} from '@angular/core';
+
+@Component({
+  selector:'app-navbar',
+  standalone:true,
+  template:`<h1>Navbar Component</h1>
+            <p>Dummy Paragraph</p>
+            <button>Submit</button>`
+})
+
+ export class AppNavbar{
+
+}
