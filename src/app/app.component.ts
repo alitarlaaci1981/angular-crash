34e12d9f27375efed6f8339f67e9d6b71a582c4b
ali <kemal_tar@hotmail.com>
2024-08-17 22:00:46 -0500

    directives and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import {FormsModule} from '@angular/forms';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AppNavbar],
+  imports: [RouterOutlet,AppNavbar,HeaderComponent,FormsModule,NgIf,NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+//  title:string ='this loaded dynamically';
+//  imgUrl:string='https://d585tldpucybw.cloudfront.net/sfimages/default-source/blogs/2023/2023-11/angular-logo-1200-303.png?sfvrsn=d8bbe35c_3';
+//  isDisabled:boolean=true;
+//  isActive:boolean=true;
+//  fruitName:string='Apple';
+
+//  userName:string='John Doe';
+
+//  buttonClick(){
+//   console.log('Button Clicked')
+//  }
+
+//  keyEnter(event:any){
+//   //console.log(event.keyCode);
+//   if(event.keyCode==13){
+//     console.log('Enter key pressed')
+//   }
+//  }
+
+//  keyupFiltering(user:HTMLInputElement){
+//   console.log(user.value)
+//  }
+
+//  updateUserName(username:HTMLInputElement){
+//   this.userName=username.value;
+//   console.log(this.userName);
+//  }
+
+isLoggedIn:boolean=false;
+userName:string='John Doe';
+
+isAdmin:boolean=false;
+isMember:boolean=false;
+isGuest:boolean=true;
+
+userRole:string='Admin';
+
+
+
+loginCount:number=0;
+countLoginAttempts(){
+  this.loginCount++;
+  console.log(this.loginCount)
+}
+
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,18 @@ import{Component} from '@angular/core';
 @Component({
   selector:'app-navbar',
   standalone:true,
-  template:`<h1>Navbar Component</h1>
-            <p>Dummy Paragraph</p>
-            <button>Submit</button>`
+  // template:`<h1>Navbar Component</h1>
+  //           <p>Dummy Paragraph</p>
+  //           <button>Submit</button>`,
+  // styles:`h1{
+  //         color:red;
+  //         background:black;
+
+  //         }
+  //         `
+  //,
+  templateUrl:'./navbar.component.html',
+  styleUrl:'./navbar.component.css'
 })
 
  export class AppNavbar{
